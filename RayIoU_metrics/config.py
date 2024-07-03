e760ae4577fe68d762d21665a246b7070f002320
YANG-CY-163 <1634640100@qq.com>
2024-07-03 14:52:36 +0800

    init RayIoU_metrics

---
+++ RayIoU_metrics/config.py
@@ -0,0 +1,16 @@
+occ3d_class_names = [
+    'others', 'barrier', 'bicycle', 'bus', 'car', 'construction_vehicle',
+    'motorcycle', 'pedestrian', 'traffic_cone', 'trailer', 'truck',
+    'driveable_surface', 'other_flat', 'sidewalk',
+    'terrain', 'manmade', 'vegetation', 'free'
+]
+
+openocc_class_names = [
+    'car', 'truck', 'trailer', 'bus', 'construction_vehicle',
+    'bicycle', 'motorcycle', 'pedestrian', 'traffic_cone', 'barrier',
+    'driveable_surface', 'other_flat', 'sidewalk',
+    'terrain', 'manmade', 'vegetation', 'free'
+]
+
+_pc_range = [-40, -40, -1.0, 40, 40, 5.4]
+_voxel_size = 0.4
